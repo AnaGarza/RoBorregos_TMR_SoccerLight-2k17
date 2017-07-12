bde56879467a83239ec282d876675b9af5a3eae4
Aurora Tijerina <auro.tj@gmail.com>
2017-07-11 22:42:07 -0500

    Add files via upload

---
+++ Acomodo_angular.ino
@@ -0,0 +1,23 @@
+//Acomoda al robot si esta desalineado del angulo inicial
+
+void acomodoAngular(int d){
+  
+  int h = d;
+  //Si el robot no se encuentra dentro del rango designado se activa el algoritmo de acomodo
+  while(h < 345 && h > 15){
+//    h=euler.x();
+    if(h<180){
+      movimientoAngular(98);
+      delay(25);
+    }
+    else{
+      movimientoAngular(99);
+      delay(25);
+    }
+    movimientoAngular(0);
+  delay(100);
+    euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    h=euler.x();
+  }
+}
+
